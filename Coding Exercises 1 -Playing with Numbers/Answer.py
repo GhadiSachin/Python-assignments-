8b895ed14b3bfd4806091cb84c7cbe93a2c0d962
Sachin Ghadi <14975366+GhadiSachin@users.noreply.github.com>
2025-12-25 09:53:46 +0530

    Add files via upload
    Coding Exercises 1 -Playing with Numbers Checkin

---
+++ Coding Exercises 1 -Playing with Numbers/Answer.py
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .SFNS-Regular_wdth_opsz14BD9E_GRAD_wght1F40000;}
+{\colortbl;\red255\green255\blue255;\red31\green32\blue48;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c16471\c16863\c24706;\cssrgb\c100000\c100000\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww33380\viewh18000\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs41\fsmilli20741 \cf2 \cb3 \expnd0\expndtw0\kerning0
+a = 10 \
+b = 5\
+print(a + b)\
+print(a - b)\
+print(a * b)\
+print(a / b)\
+print(a // b)\
+print(a % b)}
---
+++ Coding Exercises 1 -Playing with Numbers/Question.py
@@ -0,0 +1,32 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .SFNS-Regular_wdth_opsz14BD9E_GRAD_wght1F40000;\f1\fnil\fcharset0 .SFNS-Regular_wdth_opsz110000_GRAD_wght12C0000;}
+{\colortbl;\red255\green255\blue255;\red31\green32\blue48;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c16471\c16863\c24706;\cssrgb\c100000\c100000\c100000;}
+{\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc23\levelnfcn23\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{disc\}}{\leveltext\leveltemplateid1\'01\uc0\u8226 ;}{\levelnumbers;}\fi-360\li720\lin720 }{\listname ;}\listid1}}
+{\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
+\paperw11900\paperh16840\margl1440\margr1440\vieww33380\viewh18000\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs41\fsmilli20741 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Playing with Numbers\
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf2 Write a Python program that performs the following operations and prints the results\cb1 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\partightenfactor0
+\ls1\ilvl0\cf2 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Addition\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Subtraction\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Multiplication\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Division (float result)\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Floor Division (integer result)\cb1 \
+\ls1\ilvl0\cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 Modulus (remainder)\cb1 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720\partightenfactor0
+\cf2 \
+\
+#\cb3 Understanding Arithmetic Operators in Python.}
