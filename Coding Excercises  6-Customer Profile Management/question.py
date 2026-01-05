8eded54cdd59e106d8997fcf1c525b890b98d49b
Sachin Ghadi <14975366+GhadiSachin@users.noreply.github.com>
2026-01-05 18:44:02 +0530

    Add files via upload
    Coding Excercises 6-Customer Profile Management Checkin

---
+++ Coding Excercises  6-Customer Profile Management/question.py
@@ -0,0 +1,40 @@
+Customer Profile Management
+You are building a customer profile manager for a CRM (Customer Relationship Management) system. You need to store and manipulate customer data using Python dictionaries.
+
+Tasks:
+
+Create a dictionary named customer with the following fields:
+
+"name": "John Doe"
+
+"age": 32
+
+"city": "New York"
+
+Print the dictionary.
+
+Add "email" and "phone" to the dictionary.
+
+Print the updated dictionary.
+
+Print the customer’s "name" and "city" values.
+
+Check whether the key "email" exists in the dictionary and print the result.
+
+Delete the "age" field from the dictionary.
+
+Print the updated dictionary.
+
+Print all dictionary keys, values, and items.
+
+Remove and print the last inserted key-value pair.
+
+Use .get() to access the key "membership" (which doesn’t exist).
+
+Print the result.
+
+Update the dictionary with a new field "address" set to "221B Baker Street".
+
+Print the final dictionary.
+
+#Understanding Dictionary in Python
